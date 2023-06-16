2d0a55aaa6baeb2510acf2f7cdd3d2372be04445
exornamb <exornamb@gmail.com>
2023-06-16 06:43:03 -0700

    m

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 /**
- * main - main function
- *
- * Return: returns 0
- */
+* main - main function
+*
+* Return: returns 0
+*/
 int main(void)
 {
-	int i;
-	int j;
-	int k;
-	for (i = 0; i < 7; i++) {
-        for (j = i + 1; j < 8; j++) {
-        for (k = j + 1; k < 9; k++) {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
-
-                if (i < 6 || j < 7 || k < 8) {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    return 0;
+int i;
+int j;
+int k;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+for (k = 0; k <= 9; k++)
+{
+if (k > j && j > i)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
