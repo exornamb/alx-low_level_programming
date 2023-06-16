1584cd96d7ee913176ed2e4255e3d10253f320e9
exornamb <exornamb@gmail.com>
2023-06-16 07:15:19 -0700

    po

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 /**
- * main - main function
- *
- * Return: always 0
- */
-int main(void) {
-    int i;
-
-    for (i = 0; i < 16; i++) {
-        if (i < 10) {
-            putchar('0' + i);
-        } else {
-            putchar('a' + (i - 10));
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+* main - main function
+*
+* Return: always 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 16; i++)
+{
+if (i < 10)
+{
+putchar('0' + i);
+}
+else
+{
+putchar('a' + (i - 10));
+}
+}
+putchar('\n');
+return (0);
 }
