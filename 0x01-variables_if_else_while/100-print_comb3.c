3f0de9b13383a5482b1c0b57133d0267afee3462
exornamb <exornamb@gmail.com>
2023-06-16 06:54:06 -0700

    k

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 /**
- * main - main function
- *
- * Return: returns 0
- */
+* main - main function
+*
+* Return: returns 0
+*/
 int main(void)
 {
-	int i, j;
-	for (i = 10; i <= 19; i++){
-		for (j = 10; j <= 19; j++){
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
-			if (i != 19 ||  j != 19){
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
-}	
+int i;
+int j;
+for (i = 10; i <= 19; i++)
+{
+for (j = 10; j <= 19; j++)
+{
+if ((j % 10) > (i % 10))
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+if (i != 18 || j != 19)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
