8e52652e99cd0673bf38c7f53d5c00c9e6677d2d
exornamb <exornamb@gmail.com>
2023-06-20 15:06:21 -0700

    k

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,14 +6,14 @@
 int main(void)
 {
 int counter;
-unsigned long long fibonacci[100];
+unsigned long fibonacci[100];
 fibonacci[0] = 1;
 fibonacci[1] = 2;
-printf("%llu, %llu", fibonacci[0], fibonacci[1]);
+printf("%lu, %lu", fibonacci[0], fibonacci[1]);
 for (counter = 2; counter < 98; counter++)
 {
 fibonacci[counter] = fibonacci[counter - 1] + fibonacci[counter - 2];
-printf(", %llu", fibonacci[counter]);
+printf(", %lu", fibonacci[counter]);
 }
 printf("\n");
 return 0;
