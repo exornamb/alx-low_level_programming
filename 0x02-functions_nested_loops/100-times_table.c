8cef042d45ca72bfc24dd89475b2bee2fb65eae3
exornamb <exornamb@gmail.com>
2023-06-20 13:50:33 -0700

    k

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,27 +1,52 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-* print_times_table - prints the n times table, starting with 0
-* @n: the number of rows and columns in the times table
+* print_times_table - prints the times table
+* @n: integer for which the times table will be printed
+* Description: prints the times table
+* Return: void
 */
+
 void print_times_table(int n)
 {
-int row, col, result;
-if (n < 0 || n > 15)
-return;
+int row, column, product;
+
+if (n >= 0 && n < 15)
+{
 for (row = 0; row <= n; row++)
 {
-for (col = 0; col <= n; col++)
+for (column = 0; column <= n; column++)
 {
-result = row * col;
-if (col != 0)
+product = row * column;
+
+if (column == 0)
+_putchar('0');
+else if (product < 10)
 {
-printf(", ");
-if (result < 10)
-printf(" ");
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(product % 10 + '0');
 }
-printf("%d", result);
+else if (product >= 10 && product < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((product / 10) % 10 + '0');
+_putchar(product % 10 + '0');
 }
-printf("\n");
+else if (product > 99 && product < 1000)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(product / 100 + '0');
+_putchar((product / 10) % 10 + '0');
+_putchar(product % 10 + '0');
+}
+}
+_putchar('\n');
+}
 }
 }
