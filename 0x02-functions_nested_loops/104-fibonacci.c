58099b083559e2dc12a29d05e9d9385ac1c0c04b
exornamb <exornamb@gmail.com>
2023-06-20 14:30:06 -0700

    k

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
-* main - print the first 98
+* main - print the first 98 Fibonacci numbers, starting with
+* 1 and 2, separated by a comma followed by a space.
 * Return - returns 0
 */
 int main(void)
@@ -8,11 +9,11 @@ int main(void)
 int c;
 unsigned long f1 = 0, f2 = 1, s;
 unsigned long f1h1, f1h2, f2h1, f2h2;
-unsigned long h1 , h2;
+unsigned long h1, h2;
 for (c = 0; c < 92; c++)
 {
 s = f1 + f2;
-printf("%lu, ",s);
+printf("%lu, ", s);
 f1 = f2;
 f2 = s;
 }
@@ -23,12 +24,12 @@ f2h2 = f2 % 10000000000;
 for (c = 93; c < 99; c++)
 h1 = f1h1 + f2h1;
 h2 = f1h2 + f2h2;
-if(f1h2 + f2h2 > 9999999999)
+if (f1h2 + f2h2 > 9999999999)
 {
 h1 += 1;
 h2 %= 10000000000;
-printf("%lu%lu",h1,h2);
-if(c != 98)
+printf("%lu%lu", h1, h2);
+if (c != 98)
 printf(", ");
 f1h1 = f2h1;
 f1h2 = f2h2;
