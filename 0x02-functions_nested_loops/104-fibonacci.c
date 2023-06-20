92376f0de3081e9868011e8f0f54fc220fb2bcd5
exornamb <exornamb@gmail.com>
2023-06-20 15:02:52 -0700

    g

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
-
 /**
- * main - main function
- *
- * Return: nothing
- */
+* main - main function
+* Return: nothing
+*/
 int main(void)
 {
-    int counter;
-    int fibonacci[100];
-
-    fibonacci[0] = 1;
-    fibonacci[1] = 2;
-
-    printf("%d, %d", fibonacci[0], fibonacci[1]);
-
-    for (counter = 2; counter < 98; counter++)
-    {
-        fibonacci[counter] = fibonacci[counter - 1] + fibonacci[counter - 2];
-        printf(", %d", fibonacci[counter]);
-    }
-
-    printf("\n");
-
-    return 0;
+int counter;
+unsigned long long fibonacci[100];
+fibonacci[0] = 1;
+fibonacci[1] = 2;
+printf("%llu, %llu", fibonacci[0], fibonacci[1]);
+for (counter = 2; counter < 98; counter++)
+{
+fibonacci[counter] = fibonacci[counter - 1] + fibonacci[counter - 2];
+printf(", %llu", fibonacci[counter]);
+}
+printf("\n");
+return 0;
 }
