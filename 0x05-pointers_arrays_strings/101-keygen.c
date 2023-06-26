5d110ba3dd78b338615e89a7dcb84f112c1ed93f
exornamb <exornamb@gmail.com>
2023-06-26 08:26:37 -0700

    f

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,52 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-#define PASSWORD_LENGTH 13
-
-char generate_random_char(void)
-{
-    int random_num = rand() % 62;
-    if (random_num < 26)
-    {
-        return 'A' + random_num;
-    }
-    else if (random_num < 52)
-    {
-        return 'a' + (random_num - 26);
-    }
-    else
-    {
-        return '0' + (random_num - 52);
-    }
-}
-
-char *generate_password(void)
-{
-    char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
-    if (password == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed.\n");
-        exit(1);
-    }
-
-    for (int i = 0; i < PASSWORD_LENGTH; i++)
-    {
-        password[i] = generate_random_char();
-    }
-    password[PASSWORD_LENGTH] = '\0';
-
-    return password;
-}
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-    srand(time(NULL));
-
-    char *password = generate_password();
-
-    printf("%s", password);
-
-    free(password);
-    return 0;
+int i, sum;
+char c;
+srand(time(NULL));
+while (sum <= 2645)
+{
+c = rand() % 128;
+sum += c;
+putchar(c);
+}
+putchar(2772 - sum);
+return (0);
 }
