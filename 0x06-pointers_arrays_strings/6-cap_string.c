7e28b14a2621a5a8d01b76f4bccb0884704083b4
exornamb <exornamb@gmail.com>
2023-06-29 06:48:17 -0700

    k

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,33 @@
 #include "main.h"
-
 /**
- * cap_string - capitalizes all words of a string.
- * @s: String to capitalize
- * Return: Pointer to capitalized string
- */
-
+ *cap_string - capitalizes every first letter of a word in a string.
+ * @s: pointer to string.
+ * Return: pointer to s.
+*/
 char *cap_string(char *s)
 {
-char cap[14] = {' ', '\t', '\n', ',', ';', '.',
-'!', '?', '"', '(', ')', '{', '}'};
-int i;
-int j;
-i = 0;
-j = 0;
-if (i == 0 && *(s + i) >= 97 && *(s + i) <= 122)
-*(s + i) = *(s + i) - 32;
-while (*(s + i) != '\0')
-{
-while (*(cap + j) != '\0')
-{
-if (*(s + i) == *(cap + j))
-{
-if (*(s + (i + 1)) >= 97 && *(s + (i + 1)) <= 122)
-*(s + (i + 1)) = *(s + (i + 1)) - 32;
-}
-j++;
-}
-i++;
-j = 0;
-}
-return (s);
+	int count;
+
+/*  scan through string */
+	count = 0;
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
+	}
+	return (s);
 }
