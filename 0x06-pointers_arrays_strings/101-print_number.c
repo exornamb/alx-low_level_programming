1e6adfa1c3ac32467c26b10878a01d6d12720c5e
exornamb <exornamb@gmail.com>
2023-06-29 06:36:09 -0700

    pls

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,9 +6,12 @@
 */
 void print_number(int n)
 {
-  if (n / 10)
-  {
-    print_number(n / 10);
-  }
-  _putchar((n % 10) + '0');
+    if (n < 0)
+    {
+        putchar('-');
+        n = -n;
+    }
+    if (n / 10)
+        print_number(n / 10);
+    putchar(n % 10 + '0');
 }
