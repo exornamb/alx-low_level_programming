f9bf316112f2c98e930239cc6a2874e051f5f59e
exornamb <exornamb@gmail.com>
2023-06-29 08:56:16 -0700

    p

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,16 @@
  */
 void print_number(int n)
 {
-	unsigned int num;
-/*check if number is negative*/
-	num = n;
-	if (n < 0)
-	{
-		_putchar(45);
-		num = -n;
-	}
-/* print number by recursion*/
-	if (num / 10)
-	{
-		print_number(num / 10);
-	}
-	_putchar((num % 10) + '0');
+unsigned int num;
+num = n;
+if (n < 0 )
+{
+        _putchar('-');
+        num = -n;
+}
+if (num / 10 != 0)
+{
+        print_number(num / 10);
+}
+_putchar((num % 10) + '0');
 }
