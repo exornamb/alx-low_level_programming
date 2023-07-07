c9e094b68c3f4b13b8ee83d5c8d993d202d591e7
exornamb <exornamb@gmail.com>
2023-07-07 05:53:25 -0700

    all

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns length;
+ */
+int _strlen(char *s)
+{
+int i, j;
+j = 0;
+for (i = 0; s[i] != '\0'; i++)
+j++;
+
+return (j);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _abs - function that computes the absolute value
+* @n: function
+* Return: + if n < 0
+*/
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
